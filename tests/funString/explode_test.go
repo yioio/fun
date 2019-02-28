21971bdaede15315c272cb259937bc616d84abfc
陈鲁彬 <yumoop@163.com>
2019-02-28 17:59:32 +0800

    fix tests

--- tests/funString/explode_test.go
+++ tests/funString/explode_test.go
@@ -9,8 +9,9 @@ import (
 func Test_Explode(t *testing.T) {
 
 	Convey("使用一个字符串分割另一个字符串", t, func() {
-
-		So(funString.Explode("|", "abc|bcd|aaa"), ShouldEqual, []string{"abc","bcd","aaa"})
-
+		ret := funString.Explode("|", "abc|bcd|aaa")
+		So(ret[0], ShouldEqual, []string{"abc","bcd","aaa"}[0])
+		So(ret[1], ShouldEqual, []string{"abc","bcd","aaa"}[1])
+		So(ret[2], ShouldEqual, []string{"abc","bcd","aaa"}[2])
 	})
 }
